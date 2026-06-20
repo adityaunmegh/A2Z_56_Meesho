479a7a39c6c56265a9ebebf48f6483a7282626d5
JAWALERAHUL <_github_email@gmail.com>
2026-06-20 12:39:17 +0530

    fixed merge conflicts in app.js db.js and user routes

--- app.js
+++ app.js
@@ -1,56 +1,32 @@
-var express=require('express');
-<<<<<<< HEAD
-var app=express();
-var session=require('express-session');
-var fileupload=require('express-fileupload');
+var express = require('express');
+var app = express();
 
+var session = require('express-session');
+var fileupload = require('express-fileupload');
+require('dotenv').config();
+
+var setupDatabase = require('./dbSetup');
 var admin = require('./routes/admin.js');
 var user = require('./routes/user.js');
-
-//middle
-=======
-    var fileupload=require('express-fileupload');
-    var session=require('express-session');
-    require('dotenv').config();
-    console.log(process.env.EMAIL_USER);
-    var setupDatabase = require('./dbSetup');
-
-var app = express();
 
 // Initialize database on startup
 setupDatabase();
 
->>>>>>> 6b8deb8592b820908f5f32245d02a88565deaee3
-app.set('view engine','ejs');
+// Middleware
+app.set('view engine', 'ejs');
 app.use(express.static('public'));
-app.use(express.urlencoded({extended:true}));
+app.use(express.urlencoded({ extended: true }));
 app.use(fileupload());
 app.use(session({
-<<<<<<< HEAD
-    secret:'mysecrate',
-    resave:'false',
-    saveUninitialized:true
-}))
-
-
-app.use('/',user);
-//app.use('/admin',admin);
-
-app.listen(3000);
-
-
-
-
-=======
     secret: 'mysecretkey',
     resave: false,
-     saveUninitialized: true
+    saveUninitialized: true
 }));
-var user=require('./routes/user.js');
-var admin=require('./routes/admin.js');
 
-app.use('/',user);
-app.use('/admin',admin);
+// Routes
+app.use('/', user);
+app.use('/admin', admin);
 
-app.listen(3000);
->>>>>>> 6b8deb8592b820908f5f32245d02a88565deaee3
+app.listen(3000, () => {
+    console.log("Server running on http://localhost:3000");
+});
--- db.js
+++ db.js
@@ -1,25 +1,11 @@
-var mysql=require('mysql2');
-<<<<<<< HEAD
-var util=require('util');
+var mysql = require('mysql2');
 
-var conn=mysql.createConnection({
-    host:'localhost',
-    user:'root',
-    password:'ROOT',
-    database:'meesho'
-});
-
-var exe=util.promisify(conn.query).bind(conn);
-
-module.exports=exe;
-=======
-
-var connection=mysql.createConnection({
+var connection = mysql.createConnection({
     host: process.env.DB_HOST || 'localhost',
     user: process.env.DB_USER || 'root',
-    password: process.env.DB_PASS || 'root',
+    password: process.env.DB_PASS || 'ROOT',
     database: process.env.DB_NAME || 'meesho'
-}); 
+});
 
 function exe(sql, values) {
     return connection.promise().query(sql, values).then(function(result) {
@@ -27,5 +13,4 @@ function exe(sql, values) {
     });
 }
 
-module.exports=exe;
->>>>>>> 6b8deb8592b820908f5f32245d02a88565deaee3
+module.exports = exe;
